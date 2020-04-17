741a091c91a16ce1c0919cfe9d0c3dc343a117ca
ksj686 <kim_se_joon@naver.com>
2020-04-17 17:18:44 +0900

    *푸는중 9935 - 문자열 폭발

--- string/B_9935.java
+++ string/B_9935.java
@@ -5,31 +5,55 @@ import java.util.Scanner;
 //9935 - 문자열 폭발
 
 public class B_9935 {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		String bomb = sc.next();
+		int bLength = bomb.length();
+		StringBuffer result = new StringBuffer();
 
+		
+		//str.substring(0, bLength-1); 비교해보기
+		for(int  i = 0; i < bLength-1; i++) {
+			result.append(str.charAt(i));
+		}
+		
+		for (int i=bLength-1;i<str.length();i++) {
+			if (str.charAt(i) == bomb.charAt(bLength-1) && result.length() >= bLength-1) {
+				if ((result.substring(result.length()-bLength+1)).equals(bomb)){
+					result.delete(result.length()-bLength+1, result.length());
+				} else 
+					result.append(str.charAt(i));
+			} else 
+				result.append(str.charAt(i));
+		}
+
+		if (result.length()>0) {
+			System.out.println(result);
+		} else {
+			System.out.println("FRULA");
+		}
+	}
+
+	
+	//메모리 초과 풀이
+	/*
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-		int result=0;
-		int[]alpha=new int[26];
-		int num=3;
+		String str = sc.nextLine();
+		String bomb = sc.nextLine();
 		
-		for(int i=0;i<23;i+=3) {
-			//한칸당 알파벳 보통 3개씩 있으므로 예외 case를 제외한 나머지는 통일해준다.
-			for(int j=0;j<3;j++) {
-				alpha[i+j]=num;
-			}
-			//4개씩 있는 칸은 한번더
-			if(i==15 || i==22) alpha[(i++)+3]=num;
-			num++;
+		while (str.contains(bomb)) {
+			str = str.replaceAll(bomb, "");
 		}
 		
-		alpha[25]=10;
-
-		String str=sc.nextLine();
-		
-		for(int i=0;i<str.length();i++) {
-			result+=alpha[str.charAt(i)-65];
+		if (str.isEmpty()) {
+			System.out.println("FRULA");
+		} else {
+			System.out.println(str);
 		}
-		System.out.println(result);
 	}
+	*/
 }
